f487d2fa55f3e6db5820dc7d790d385c3d15f2bd
ManashJBofficial <mjbaruah10@gmail.com>
2020-08-27 13:04:03 +0530

    Updates

---
+++ precache-manifest.4bb6df448b7d4a40baf674a7ba726258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51049088fd2f03597db789a1ab915c1a",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "69dcfe48ad77d527dcc9",
+    "url": "/monster-app/static/css/main.aeff4456.chunk.css"
+  },
+  {
+    "revision": "52ab36c179d2e860984b",
+    "url": "/monster-app/static/js/2.1662debb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.1662debb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69dcfe48ad77d527dcc9",
+    "url": "/monster-app/static/js/main.87e509c3.chunk.js"
+  },
+  {
+    "revision": "0c571607ebdb9a69c537",
+    "url": "/monster-app/static/js/runtime-main.0e395675.js"
+  }
+]);
